c0d271ad16e5a43cb322e289d1cfa3c4517c2ac2
ant <anthonybui@gmail.com>
2013-10-29 23:53:40 -0400

    working non-js flow

--- content-quiz.php
+++ content-quiz.php
@@ -1,111 +1,91 @@
 <?php
-/**
- * The template for displaying individual quizzes
- */
-?>
+   /**
+   * The template for displaying individual quizzes
+   */
+   ?>
 
 <?php
 
-global $question_id;
+   global $question_id;
 
-// post results
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && false) {
-	pr($_POST);
-} else {
-	// get current question index (question index will always be in the url)
-	// $question_id = ((int) $_GET['Q']) || 1;
-	
-	$question_id = (isset($_GET['Q'])) ? (int) $_GET['Q'] : 1;
+   $post_meta = get_fields();
+   $quiz = new PersonalityQuiz($post->ID, $post_meta);
 
-	// git all questions and put into appropriate arrays
-	$post_meta = array_filter(get_fields(), "trim");
-	$results_array = array();
-	$questions_array = array();
-	foreach ($post_meta as $key => $text) {
-		if (preg_match("/^result_(\d)/", $key,  $matches)) {
-			$results_array[$matches[1]] = $text;
-		}
-		if (preg_match("/^question.*/", $key)) {
-			if (preg_match("/^question_(\d)$/", $key, $matches)) {
-				$qid = $matches[1];
-				$questions_array[$qid]['question'] = $text;
-			} else if (preg_match("/^question_\d_answer_(\d)$/", $key, $matches)) {
-				$questions_array[$qid]['answers'][$matches[1]] = $text;
-			}
-		}
-	}
+   $question_id = (isset($_GET['q'])) ? (int) $_GET['q'] : 0;
 
-	// pr($results_array);
-	sort($questions_array);
-	// pr($questions_array);
-	// echo '<hr/>';
-
+   // post results
+   if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_GET['results'] == true) {
+     $result = $quiz->get_result(array_values($_POST));
+     pr($_POST);
+pr($result);
+   } else {
 ?>
 
-<form class="quiz" action="<?php echo get_action_url($question_id+1) ?>" method="POST">
-	<!-- loop through and output questions -->
-	<?php pr($_GET) ?>
-	<?php pr($_POST) ?>
-	<?php pr($question_id) ?>
-	<?php foreach ($questions_array as $qid => $question) { ?>
-	<div class="question <? if ($qid+1 == $question_id) echo 'active'; ?>">
-		<h3><?php echo $question['question'] ?></h3>
-		
-		<div class="question-answers">
-			<!-- loop through and output answers! -->
-			<?php foreach ($question['answers'] as $aid => $answer) { ?>
-				<label class="question-answer radio">
-					<input type="radio" class="no-js" name="question_<?php echo $qid ?>" id="question_<?php echo $qid ?>_answer_<?php echo $aid ?>" value="<?php echo $aid ?>">
-					<?php echo $answer ?>
-				</label>
-			<?php } ?>
-		</div>
-	</div>
-	<?php } ?>
+<form class="quiz" action="<?php echo $quiz->get_action_url($question_id+1) ?>" method="POST">
 
-	<!-- loop through and add -->
+  <!-- <?php pr($_GET) ?> -->
+  <!-- <?php pr($_POST) ?> -->
 
-	<!-- next button that hides with js -->
-	<?php $value = (false) ? "Submit" : "Next &rarr;" ?>
-	<div class="quiz-controls no-js">
-		<input type="submit" value="<?php echo $value ?>">
-	</div>
+  <!-- loop through and output questions -->
+  <?php foreach ($quiz->questions as $qid => $question) { ?>
+  <div class="question <? if ($qid == $question_id) echo 'active'; ?>">
+    <h3><?php echo $question['question'] ?></h3>
 
-<!-- 	<div class="question">
-		<h3>What is your favorite color?</h3>
-		<ul>
-			<li>
-				<label class="question-answer radio">
-					<input type="radio" name="question_1" id="question_1_answer_1" value="1">
-					asdfasdfasdfasdf
-				</label>
-			</li>
-			<li>
-				<label class="question-answer radio">
-					<input type="radio" name="question_1" id="question_1_answer_2" value="2">
-					asdfasdfasdfasdf
-				</label>
-			</li>
-			<li>
-				<label class="question-answer radio">
-					<input type="radio" name="question_1" id="question_1_answer_3" value="3">
-					awlahja;eljglasdkjflakjwef
-				</label>
-			</li>
-			<li>
-				<label class="question-answer radio">
-					<input type="radio" name="question_1" id="question_1_answer_4" value="4">
-					this a;lksdjf;lawje;fljaa w;lejflawjefl;kajwef;lkjaw;elfjl
-					;awekjf;lakjdsfansvanw,mn,mn,mn,mrnt,mrntm,
-				</label>
-			</li>
-			<li>
-				<label class="question-answer radio">
-					<input type="radio" name="question_1" id="question_1_answer_5" value="5">
-					a
-				</label>
-			</li>
-		</ul>
-	</div> -->
+    <div class="question-answers">
+      <!-- loop through and output answers -->
+      <?php foreach ($question['answers'] as $aid => $answer) { ?>
+      <label class="question-answer radio">
+        <input type="radio" class="no-js" <?php is_checked($_POST['question_'.$qid], $answer['answer_result_ids']) ?> name="question_<?php echo $qid ?>" id="question_<?php echo $qid ?>_answer_<?php echo $aid ?>" value="<?php echo $answer['answer_result_ids'] ?>"/>
+        <?php echo $answer['answer_text'] ?>
+      </label>
+      <?php } ?>
+    </div>
+  </div>
+  <?php } ?>
+
+  <!-- loop through and add -->
+
+  <!-- next button that hides with js -->
+  <?php $value = (false) ? "Submit" : "Next &rarr;" ?>
+  <div class="quiz-controls no-js">
+    <input type="submit" value="<?php echo $value ?>">
+  </div>
+
+  <!--       <div class="question">
+             <h3>What is your favorite color?</h3>
+             <ul>
+               <li>
+                 <label class="question-answer radio">
+                   <input type="radio" name="question_1" id="question_1_answer_1" value="1">
+                   asdfasdfasdfasdf
+                 </label>
+               </li>
+               <li>
+                 <label class="question-answer radio">
+                   <input type="radio" name="question_1" id="question_1_answer_2" value="2">
+                   asdfasdfasdfasdf
+                 </label>
+               </li>
+               <li>
+                 <label class="question-answer radio">
+                   <input type="radio" name="question_1" id="question_1_answer_3" value="3">
+                   awlahja;eljglasdkjflakjwef
+                 </label>
+               </li>
+               <li>
+                 <label class="question-answer radio">
+                   <input type="radio" name="question_1" id="question_1_answer_4" value="4">
+                   this a;lksdjf;lawje;fljaa w;lejflawjefl;kajwef;lkjaw;elfjl
+                   ;awekjf;lakjdsfansvanw,mn,mn,mn,mrnt,mrntm,
+                 </label>
+               </li>
+               <li>
+                 <label class="question-answer radio">
+                   <input type="radio" name="question_1" id="question_1_answer_5" value="5">
+                   a
+                 </label>
+               </li>
+             </ul>
+    </div> -->
 </form>
 <? } ?>
--- functions.php
+++ functions.php
@@ -1,4 +1,44 @@
 <?php
+class PersonalityQuiz {
+  var $id;
+  var $questions;
+  var $results;
+
+  function PersonalityQuiz($id, $post_meta) {
+    $this->id = $id;
+    $this->questions = $post_meta['quiz_questions'];
+    $this->results = $post_meta['quiz_results'];
+  }
+
+  function get_question($id) {
+    return $this->questions[$id-1];
+  }
+
+  function get_action_url($id) {
+    if (!isset($this->questions[$id])) {
+	return "?p=" . $this->id . "&results=true";
+    }
+    return "?p=" . $this->id . "&q=" . $id;
+  }
+
+  function render_question($qid) {
+     echo '
+       <label class="question-answer radio">
+         <input type="radio" class="no-js" name="question_' . $qid . '" id="question_'. $qid . '_answer_' . $aid . '" value="' . $answer['answer_result_ids'] . '"/>
+        ' . $answer['answer_text'] . '
+       </label>
+          ';
+  }
+
+  function get_result($list) {
+    $values = array_count_values($list);
+    $rid = (array_search(max($values), $values));
+
+    return $this->results[$rid];
+    // TODO do something special to randomize ties?
+  }
+}
+
 
 // function to pretty print arrays
 function pr($array=null) {
@@ -9,6 +49,10 @@ function pr($array=null) {
 /*  Enqueues scripts for front-end
 /*-----------------------------------------------------------------------------------*/
 add_action('wp_enqueue_scripts', 'child_add_scripts');
+
+function is_checked($a, $b) {
+  echo ($a == $b) ? 'checked=checked' : '';
+}
 
 function child_add_scripts() {
 	wp_register_script(
@@ -21,8 +65,8 @@ function child_add_scripts() {
 	wp_enqueue_script( 'brainfall', array('jquery') );
 }
 
-function get_action_url($q_id) {
-	return "?Q=" . $q_id;
+function get_action_url($quiz_id, $question_id) {
+	return "?p=" . $quiz_id . "&q=" . $question_id;
 }
 
 ?>
